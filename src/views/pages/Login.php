3ce1d8eb2bc433e57849e114de0a931d8f344781
Mohamed <mohameddrame907@gmail.com>
2022-03-03 13:58:59 +0100

    wip formLogin

--- src/views/pages/Login.php
+++ src/views/pages/Login.php
@@ -3,3 +3,50 @@
 <?php
 include __DIR__."../../partials/Header.php";
 ?>
+
+<div class= "framework">
+    <div class="contentForm">
+    <div class="signup">
+        <h3>Signup</h3>
+        <form>
+            <div class="classMailSignup">
+                <label for="Email">Email</label>
+                <input type="email" name="mailSignup">
+            </div>
+
+            <div class="classPseudoSingnup">
+                <label for="Pseudo">Pseudo</label>
+                <input type="text" name="pseudoSignup">
+            </div>
+            
+            <div class="classPasswordSignup">
+                <label for="Password">Password</label>
+                <input type="password" name="passwordSignup">
+            </div>
+
+            <div class="submitbtn">
+                <input type="submit" value="Send">
+            </div>
+        </form>
+    </div>
+
+    <div class="login">
+        <h3>Login</h3>        
+        <div class="classPseudoLogin">
+            <label for="Pseudo">Pseudo </label>
+            <input type="text" name="pseudoLog">
+        </div>
+            <div class="classPasswordLogin">
+                <label for="Password">Password</label>
+                <input type="password" name="passwordLog">
+            </div>
+        <div class="submitbtn">
+            <input type="submit" value="Send">
+        </div>
+    </div>
+    
+        
+      
+    </div>
+    
+</div>
