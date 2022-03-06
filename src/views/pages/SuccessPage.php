d008ca8fb9c66a89ef02147aad9e2588fa1d952c
Mohamed <mohameddrame907@gmail.com>
2022-03-06 15:34:18 +0100

    pseudo appear when the account is created

--- src/views/pages/SuccessPage.php
+++ src/views/pages/SuccessPage.php
@@ -6,7 +6,7 @@ include __DIR__."../../partials/Header.php";
 
 <div class="generalContent">
     <div class = "contentSuccess" />
-        <h3> Welcome in the team </br> "Username"</h3>    <!--<?= $_SESSION["username"]?> -->
+        <h3> Welcome in the team </br>  <?= $_SESSION["username"]?> </h3>   
         <div class="images">
             <img class ="firstImg" src="ressources/congratulation.png" alt="congratsImg">
             <img class ="secondImg" src="ressources/congratulation.png" alt="congratsImg">
